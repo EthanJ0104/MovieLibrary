f0374f0a765985a19d4d81fa307240a2e953661c
Ethan Jaworski <edjaworsk@gmail.com>
2023-02-26 17:18:36 -0600

    Read CSV Finished

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using NLog;
 
 string option = "1";
+string file = "movies.csv";
 
 while (option == "1" || option == "2")
 {
@@ -10,12 +11,26 @@ while (option == "1" || option == "2")
 
     if (option == "1")
     {
-
+        if (File.Exists(file))
+        {
+            StreamReader sr = new StreamReader(file);
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] arr = line.Split(',');
+                Console.WriteLine("Movie ID: {0}, Title: {1}, Genres: {2}", arr[0], arr[1], arr[2]);
+            }
+            sr.Close();
+        }
+        else
+        {
+            Console.WriteLine("File does not exist.");
+        }
     }
 
     if (option == "2")
     {
-
+        
     }
 }
 
