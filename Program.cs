ec6cc99c4408e47005888b4117291fab05af0f34
Ethan Jaworski <edjaworsk@gmail.com>
2023-02-26 15:03:09 -0600

    Created the structure of the program.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using NLog;
 
+string option = "1";
+
+while (option == "1" || option == "2")
+{
+    Console.Write("Enter 1 to view movies in the file, enter 2 to add a movie. Enter anything else to exit: ");
+    option = Console.ReadLine();
+
+    if (option == "1")
+    {
+
+    }
+
+    if (option == "2")
+    {
+
+    }
+}
+
+Console.WriteLine("Goodbye");
